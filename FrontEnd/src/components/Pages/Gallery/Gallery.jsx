084de76340eebd1e92a9d4bdbf346e081ce7f37a
Flavio Avdulla <a.flavio4366@gmail.com>
2025-01-28 23:03:30 +0100

    web_update

--- FrontEnd/src/components/Pages/Gallery/Gallery.jsx
+++ FrontEnd/src/components/Pages/Gallery/Gallery.jsx
@@ -13,37 +13,81 @@ import gallery_07 from "../../../assets/images/Gallery/gallery_07.jpg";
 import gallery_08 from "../../../assets/images/Gallery/gallery_08.jpg";
 import gallery_09 from "../../../assets/images/Gallery/gallery_09.jpg";
 import gallery_10 from "../../../assets/images/Gallery/gallery_10.jpg";
+import gallery_11 from "../../../assets/images/Gallery/gallery_11.jpg";
+
+// Icons
+import { SlArrowRight } from "react-icons/sl";
+import { SlArrowLeft } from "react-icons/sl";
+import { LuCheck } from "react-icons/lu";
+
+const images = [
+  gallery_01,
+  gallery_02,
+  gallery_03,
+  gallery_04,
+  gallery_05,
+  gallery_06,
+  gallery_07,
+  gallery_08,
+  gallery_09,
+  gallery_10,
+];
 
 // eslint-disable-next-line react/prop-types
-const Modal = ({ isOpen, src, onClose }) => {
+const Modal = ({ isOpen, currentIndex, onNext, onPrev, onClose }) => {
   if (!isOpen) return null;
 
   return (
     <div className="modal" onClick={onClose}>
-      <div className="modal-content">
-        <img src={src} alt="modal" />
+      <i
+        onClick={(e) => {
+          e.stopPropagation();
+          onPrev();
+        }}
+      >
+        <SlArrowLeft className="arrow-left" />
+      </i>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <img src={images[currentIndex]} alt="modal" />
       </div>
+      <i
+        onClick={(e) => {
+          e.stopPropagation();
+          onNext();
+        }}
+      >
+        <SlArrowRight className="arrow-right" />
+      </i>
     </div>
   );
 };
 
 const Gallery = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalImageSrc, setModalImageSrc] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleImageClick = (src) => {
-    setModalImageSrc(src);
+  const handleImageClick = (index) => {
+    setCurrentIndex(index);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setModalImageSrc("");
+  };
+
+  const goToNextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const goToPrevImage = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   return (
     <div className="gallery">
-      <div className="gallery-container">
+      <div className="gallery-container-01">
         <div className="gallery-title">
           <h1>Masonry Layout</h1>
           <p>
@@ -55,29 +99,117 @@ const Gallery = () => {
 
         <div className="gallery-photos">
           <div className="gallery-section-01">
-            <img src={gallery_01} alt="gallery_01" onClick={() => handleImageClick(gallery_01)} />
-            <img src={gallery_02} alt="gallery_02" onClick={() => handleImageClick(gallery_02)} />
+            <img
+              src={gallery_01}
+              alt="gallery_01"
+              onClick={() => handleImageClick(0)}
+            />
+            <img
+              src={gallery_02}
+              alt="gallery_02"
+              onClick={() => handleImageClick(1)}
+            />
           </div>
           <div className="gallery-section-01">
-            <img src={gallery_06} alt="gallery_06" onClick={() => handleImageClick(gallery_01)} />
-            <img src={gallery_03} alt="gallery_03" onClick={() => handleImageClick(gallery_01)} />
+            <img
+              src={gallery_06}
+              alt="gallery_06"
+              onClick={() => handleImageClick(5)}
+            />
+            <img
+              src={gallery_03}
+              alt="gallery_03"
+              onClick={() => handleImageClick(2)}
+            />
           </div>
           <div className="gallery-section-01">
-          <img src={gallery_08} alt="gallery_08" onClick={() => handleImageClick(gallery_02)} />
-          <img src={gallery_07} alt="gallery_07" onClick={() => handleImageClick(gallery_01)} />
+            <img
+              src={gallery_08}
+              alt="gallery_08"
+              onClick={() => handleImageClick(7)}
+            />
+            <img
+              src={gallery_07}
+              alt="gallery_07"
+              onClick={() => handleImageClick(6)}
+            />
           </div>
           <div className="gallery-section-01">
-            <img src={gallery_04} alt="gallery_04" onClick={() => handleImageClick(gallery_01)} />
-            <img src={gallery_05} alt="gallery_05" onClick={() => handleImageClick(gallery_01)} />
+            <img
+              src={gallery_04}
+              alt="gallery_04"
+              onClick={() => handleImageClick(3)}
+            />
+            <img
+              src={gallery_05}
+              alt="gallery_05"
+              onClick={() => handleImageClick(4)}
+            />
           </div>
           <div className="gallery-section-01">
-            <img src={gallery_10} alt="gallery_10" onClick={() => handleImageClick(gallery_01)} />
-            <img src={gallery_09} alt="gallery_09" onClick={() => handleImageClick(gallery_02)} />
+            <img
+              src={gallery_10}
+              alt="gallery_10"
+              onClick={() => handleImageClick(9)}
+            />
+            <img
+              src={gallery_09}
+              alt="gallery_09"
+              onClick={() => handleImageClick(8)}
+            />
           </div>
         </div>
       </div>
 
-      <Modal isOpen={isModalOpen} src={modalImageSrc} onClose={closeModal} />
+      <Modal
+        isOpen={isModalOpen}
+        currentIndex={currentIndex}
+        onNext={goToNextImage}
+        onPrev={goToPrevImage}
+        onClose={closeModal}
+      />
+      <div className="gallery-container-02">
+        <div className="gallery-section-02">
+          <div className="gallery-left">
+            <img src={gallery_11} alt="gallery_11" />
+          </div>
+          <div className="gallery-right">
+            <div className="gallery-right-01">argarg</div>
+            <div className="gallery-right-02">
+              <div className="gallery-right-02-a">
+                <h1>Hotel Info</h1>
+                <div className="info-details">
+                  <div className="info-details-1">
+                    <i>
+                      <LuCheck className="check-icon" />
+                    </i>
+                    <p>Drinks Included</p>
+                  </div>
+                  <div className="info-details-2">
+                    <i>
+                      <LuCheck className="check-icon" />
+                    </i>
+                    <p>Pets Allowed</p>
+                  </div>
+                  <div className="info-details-3">
+                    <i>
+                      <LuCheck className="check-icon" />
+                    </i>
+                    <p>Heated Pool</p>
+                  </div>
+                  <div className="info-details-4">
+                    <i>
+                      <LuCheck className="check-icon" />
+                    </i>
+                    <p>Indoor Big Gym</p>
+                  </div>
+                </div>
+              </div>
+              <div className="gallery-right-02-b">arewga</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
